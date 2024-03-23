506a9fc3116a6772a735e71d413eae8786b25ea1
Profility <secure@profility.slmail.me>
2024-03-23 10:11:40 +0800

    added several features

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import curses
 import random
-import time
+import json
 
 # TODO:
 # Remove delay for the cursor when pressing backspace
-# Add support for multiple y-lines
+# Add support for multiple y-lines, word wrapping
 # Add wpm and time elapsed
-# Add random words
+# Add random words /
+
+def getWords():
+    with open('wordlist.json', 'r') as list:
+        return str(' '.join(random.sample(json.load(list), 15)))
 
 def main(screen):
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK) # Correct
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED) # Wrong
+    curses.curs_set(0) # Disable cursor
 
-    wordText = 'This is snaketype, a python typing tester.'
+    wordText = getWords()
+    wrongFlag = False
     typedText = []
-    flag = False
 
     screen.keypad(True)
     screen.clear()
     screen.addstr(wordText)
     screen.refresh()
-
-    curses.curs_set(0) # Disable cursor
 
     while True:
         if ''.join(typedText) == wordText: # Break when typedtext is equal to wordtext
@@ -29,31 +32,40 @@ def main(screen):
             break
 
         key = screen.getch()
-        if key == 17: # If CTRL+Q pressed
+
+        # If CTRL+Q pressed
+        if key == 17 or key == 27:
             break
 
-        elif key != 8: # If any key pressed
+        # Handling resizing
+        elif key == curses.KEY_RESIZE:
+            curses.resize_term()
+            continue
+
+        # If any key pressed other than backspace
+        elif key != 8:
             typedText.append(chr(key))
 
             # Compare typed text with target text
             for i, c in enumerate(typedText):
                 textColor = curses.color_pair(1)
                 if c != wordText[i]: # If typed character doesn't match target
-                    flag = True
+                    wrongFlag = True
                     textColor = curses.color_pair(2)
-                elif flag == False: # If typed character matches target
+                elif wrongFlag == False: # If typed character matches target
                     textColor = curses.color_pair(1)
 
                 screen.addstr(0, i, wordText[i], textColor) # Print characters with appropriate color
                 #screen.addstr(2, 0, f"Typed: {''.join(typedText)}")
-
-        elif key == 8: # If BACKSPACE is pressed
+        
+        # If BACKSPACE is pressed
+        elif key == 8:
             if len(typedText) > 0:
                 length = len(typedText) - 1
                 typedText.pop()
                 screen.move(0, length)
                 screen.addstr(0, len(typedText), wordText[len(typedText)])
+
         screen.refresh()
             
-
 curses.wrapper(main)
